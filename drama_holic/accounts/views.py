d40b8a153cb3976f8985dfb65b85f920448a3033
skmeetaali <mitalikamble5@gmail.com>
2026-06-04 11:00:22 +0530

    ckeckout

--- drama_holic/accounts/views.py
+++ drama_holic/accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login,authenticate
 
 # Create your views here.
 class SignUpView(CreateView):
